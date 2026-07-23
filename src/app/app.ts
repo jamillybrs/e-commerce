c880f6275097a763e5c9729cf39bc230ee020936
Jamilly Barbosa da Costa <costajamilly057@gmail.com>
2026-07-22 21:39:48 -0300

    (feat) adiciona funcionalidade login e logout

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,8 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router'; // remove a importação de RouterOutlet, pois não é necessário para este componente
 
 import { UpperCasePipe } from '@angular/common';
+import { usuarioLogado, login, logout } from './core/auth';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, UpperCasePipe],
@@ -11,4 +13,8 @@ import { UpperCasePipe } from '@angular/common';
 export class App {
   protected readonly title = signal('e-commerce');
   nomeLoja = 'Xopee';
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
+  
 }
